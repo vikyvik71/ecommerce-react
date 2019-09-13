03e6cfac8c7cc23a35de9e849c58d5121e373991
Vittorio Marino <vittorio@Vittorios-MacBook-Pro.local>
2019-09-13 17:36:31 +0200

    Homepage styling and structuring

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component';
+
+const HomePage = () => (
+    <div className="homepage">
+        <DirectoryMenu />    
+    </div>
+);
+
+export default HomePage;
